c882430e845d0b30b46ff133c14eb75fda287caa
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-02-23 16:33:24 -0300

    adicionando o identity e criando migrations

---
+++ UsuariosApi/Data/UserDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsuariosApi.Data
+{
+    // precisamos definir qual contexto vamos usar 
+    // estamos usando um identity user 
+    // temos um identificador inteiro <int>
+    // e ele tem um papel dentro do sistema (uma role)
+    // e a chave usada para identificar ele em nosso sistema é um inteiro 
+    public class UserDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        // essas opções farão referencia ao nosso user db context
+        public UserDbContext(DbContextOptions<UserDbContext> opt) : base(opt)
+        {
+            
+        }
+    }
+}
--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UsuariosApi.Data;
 
 namespace UsuariosApi
 {
@@ -26,7 +29,13 @@ namespace UsuariosApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // configuração do banco 
+            services.AddDbContext<UserDbContext>(options => 
+                    options.UseMySQL(Configuration.GetConnectionString("UsuarioConnection")));
+            // configuração do uso do identity
+            // vamos usar para armazenar os dados de autenticação (AddEntityFrameworkStores)
+            services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+                    .AddEntityFrameworkStores<UserDbContext>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
